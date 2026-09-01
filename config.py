df8cfc861942a1bbb24d06e68dcd532109945564
Lucas <lucas.lougon@globalsys.com.br>
2026-09-01 17:52:03 -0300

    build: configurar dockerfile com uv para deploy em producao

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 10080
+    DATABASE_URL: str
 
     # Isso faz o Pydantic buscar as variáveis do arquivo .env
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
--- database/connection.py
+++ database/connection.py
@@ -1,11 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./cine_random.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
